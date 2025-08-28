b502dbb5f2e2d56f99f27edb3de3f052de82087a
lindasantika08 <linda.santika.tif22@polban.ac.id>
2025-08-28 19:47:12 +0700

    setup Tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="p-6">
+    <h1 class="text-3xl font-bold text-blue-600 underline">
+      Hello Tailwind + Vue 🎉
+    </h1>
+  </div>
 </template>
-
-<style scoped></style>
